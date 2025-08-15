13f257781ad17a1fc9169ad1f17c1cbfee88b53e
Your Name <abdulrahmanchibon8@gmail.com>
2025-08-15 17:42:32 +0300

    إضافة إعدادات هذا النطاق الموثوق

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,12 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**.public.blob.vercel-storage.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'jldyyfkashoisxxyfhmb.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
     ],
   },
 };
