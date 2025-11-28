f040e42b9a2f55e049ed7e8808cd83ca67ca0dc9
abdulrahmanch1 <abdulrahmanchibon8@gmail.com>
2025-11-28 09:52:33 +0300

    تم تحسين الادا

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,10 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    qualities: [60, 75], // Added to support quality={60} usage
+    qualities: [60, 75, 85],
+    minimumCacheTTL: 60,
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: 'https',
